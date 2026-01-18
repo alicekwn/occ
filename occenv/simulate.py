a2184c0bbe991cbfd0409c0be23aca209bf22672
alicekwn <111282770+alicekwn@users.noreply.github.com>
2026-01-18 02:14:18 +0000

    Add level count simulation

--- occenv/simulate.py
+++ occenv/simulate.py
@@ -1,6 +1,7 @@
 from joblib import Parallel, delayed
 from occenv.env import DataShare
 from collections import Counter
+import numpy as np
 
 
 class Simulate:
@@ -9,7 +10,34 @@ class Simulate:
         self.shard_sizes = shard_sizes
         self.m = len(shard_sizes)
 
-    def simulate_bivariate_once(self) -> tuple[int, int]:
+    def _simulate_level_count_once(self) -> list[int]:
+        """
+        Single run → level counts of the data share.
+        """
+        new_mpc = DataShare(self.total_number)
+        shards = [set(new_mpc.create_shard(s)) for s in self.shard_sizes]
+        level_counts = [0] * (self.m + 1)
+        for i in range(self.total_number):
+            count = 0
+            for shard in shards:
+                if i in shard:
+                    count += 1
+            level_counts[count] += 1
+        return level_counts
+
+    def simulate_level_count_repeat(self, repeat: int) -> list[int]:
+        """
+        Repeat the simulation of level counts and return the average level counts for each level.
+        """
+        levels = np.array(
+            Parallel(n_jobs=-1)(
+                delayed(self._simulate_level_count_once)() for _ in range(repeat)
+            )
+        )  # the output is a list of lists, each list is the level counts of a single run
+        level_average = [float(round(sum(x) / repeat, 4)) for x in zip(*levels)]
+        return level_average
+
+    def _simulate_bivariate_once(self) -> tuple[int, int]:
         """
         Single run → (U, V) for ALL shards:
         U = union size, V = intersection size.
@@ -31,15 +59,17 @@ class Simulate:
         Runs in blocks to avoid storing all raw samples.
         """
 
-        def run_block(n: int) -> Counter:
+        def _run_block(block_number: int) -> Counter:
             c = Counter()
-            for _ in range(n):
-                c[self.simulate_bivariate_once()] += 1
+            for _ in range(block_number):
+                c[self._simulate_bivariate_once()] += 1
             return c
 
         q, r = divmod(repeat, block)
         blocks = [block] * q + ([r] if r else [])
-        parts = Parallel(n_jobs=-1)(delayed(run_block)(n) for n in blocks)
+        parts = Parallel(n_jobs=-1)(
+            delayed(_run_block)(block_number) for block_number in blocks
+        )
 
         total = Counter()
         for part in parts:
@@ -47,34 +77,37 @@ class Simulate:
 
         return {uv: cnt / repeat for uv, cnt in total.items()}
 
-    def simulate_union_once(self) -> int:
-        U, _ = self.simulate_bivariate_once()
+    def _simulate_union_once(self) -> int:
+        U, _ = self._simulate_bivariate_once()
         return U
 
     def simulate_union_repeat(self, repeat: int) -> list[int]:
         return Parallel(n_jobs=-1)(
-            delayed(self.simulate_union_once)() for _ in range(repeat)
+            delayed(self._simulate_union_once)() for _ in range(repeat)
         )
 
-    def simulate_intersection_once(self) -> int:
-        _, V = self.simulate_bivariate_once()
+    def _simulate_intersection_once(self) -> int:
+        _, V = self._simulate_bivariate_once()
         return V
 
     def simulate_intersection_repeat(self, repeat: int) -> list[int]:
         return Parallel(n_jobs=-1)(
-            delayed(self.simulate_intersection_once)() for _ in range(repeat)
+            delayed(self._simulate_intersection_once)() for _ in range(repeat)
         )
 
 
 if __name__ == "__main__":
-    total_number = 100
-    shard_sizes = [50, 63, 75]
+    N = 100
+    n = [50, 63, 75]
 
-    simulator = Simulate(total_number, shard_sizes)
-    repeat = int(1e6)
+    simulator = Simulate(N, n)
+    repeats = int(1e6)
 
-    union = simulator.simulate_union_repeat(repeat)
-    print("The average union is ", sum(union) / repeat)
+    # union = simulator.simulate_union_repeat(repeats)
+    # print("The average union is ", sum(union) / repeats)
 
-    intersection = simulator.simulate_intersection_repeat(repeat)
-    print("The average intersection is ", sum(intersection) / repeat)
+    # intersection = simulator.simulate_intersection_repeat(repeats)
+    # print("The average intersection is ", sum(intersection) / repeats)
+
+    repeat_level_counts = simulator.simulate_level_count_repeat(repeats)
+    print("The average level counts are ", repeat_level_counts)
---
+++ scripts/plot_level_count.py
@@ -0,0 +1,3 @@
+"""
+After running the simulation of 1e6 times, plot the histogram of the level counts -> we can see which level l (number of elements chosen by l parties) is the most likely to happen.
+"""
